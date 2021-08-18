f6f1de814e80a1c03d79c409464516986b154dab
lwmfjc <lwmfjc@gmail.com>
2021-08-18 14:36:42 +0800

    'redis消息收发'

---
+++ src/main/java/com/example/messagingredis/Receiver.java
@@ -0,0 +1,26 @@
+package com.example.messagingredis;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * 接收消息
+ */
+public class Receiver {
+    private static final Logger LOGGER = LoggerFactory.getLogger(
+            Receiver.class
+    );
+    private AtomicInteger counter = new AtomicInteger();
+
+    public void receiveMessage(String message) {
+        LOGGER.info("Received <" + message + ">");
+        counter.incrementAndGet();
+    }
+
+    public int getCount() {
+        return counter.get();
+    }
+}
